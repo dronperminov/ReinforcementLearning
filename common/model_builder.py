aa9f2d0e9dc11e357663cdce75348ff787efc2be
dronperminov <dronperminov@gmail.com>
2022-07-10 12:34:19 +0300

    optimize building models,add build layer method

--- common/model_builder.py
+++ common/model_builder.py
@@ -1,9 +1,41 @@
-from typing import List, Union
+from typing import List, Union, Optional
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Input
 
 
 class ModelBuilder:
+    @staticmethod
+    def build_layer(config: dict, params: Optional[dict] = None):
+        if params is None:
+            params = {}
+
+        if config['type'] == 'input':
+            inputs = config['inputs']
+            params['shape'] = (inputs,) if isinstance(inputs, int) else (*inputs,)
+            return Input(**params)
+
+        if config['type'] == 'dense':
+            params['units'] = config['size']
+            params['activation'] = config.get('activation', 'linear')
+            return Dense(**params)
+
+        if config['type'] == 'conv':
+            params['filters'] = config['fc']
+            params['kernel_size'] = config['fs']
+            params['strides'] = config.get('stride', 1)
+            params['padding'] = config.get('padding', 'valid')
+            params['activation'] = config.get('activation', 'relu')
+            return Conv2D(**params)
+
+        if config['type'] == 'maxpool':
+            params['pool_size'] = config['scale']
+            return MaxPooling2D(**params)
+
+        if config['type'] == 'flatten':
+            return Flatten(**params)
+
+        raise ValueError(f'Invalid layer type "{config["type"]}"')
+
     @staticmethod
     def build(inputs: Union[int, list], architecture: List[dict]) -> Sequential:
         model = Sequential()
@@ -16,25 +48,7 @@ class ModelBuilder:
                 params['input_shape'] = (inputs,) if isinstance(inputs, int) else (*inputs,)
                 is_first = False
 
-            if config['type'] == 'dense':
-                params['units'] = config['size']
-                params['activation'] = config.get('activation', 'linear')
-                layer = Dense(**params)
-            elif config['type'] == 'conv':
-                params['filters'] = config['fc']
-                params['kernel_size'] = config['fs']
-                params['strides'] = config['stride']
-                params['padding'] = config['padding']
-                params['activation'] = config.get('activation', 'relu')
-                layer = Conv2D(**params)
-            elif config['type'] == 'maxpool':
-                params['pool_size'] = config['scale']
-                layer = MaxPooling2D(**params)
-            elif config['type'] == 'flatten':
-                layer = Flatten(**params)
-            else:
-                raise ValueError(f'Invalid layer type "{config["type"]}"')
-
+            layer = ModelBuilder.build_layer(config, params)
             model.add(layer)
 
         return model
